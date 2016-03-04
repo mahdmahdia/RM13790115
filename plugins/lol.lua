cabd9665e00175b2c4cb8c313f81cb336ec04d84
mahdmahdia <mahdi028c@gmail.com>
2016-03-04 23:05:51 +0330

    Update lol.lua

--- plugins/lol.lua
+++ plugins/lol.lua
@@ -3,14 +3,18 @@ do
 function run(msg, matches)
 local reply_id = msg['id']
 
-local info = '^_^'
+local text = ':|'
 
-reply_msg(reply_id, info, ok_cb, false)
+reply_msg(reply_id, text, ok_cb, false)
 end
 
 return {
 patterns = {
-"^>😂"
+"^😐",
+    "^😐😐",
+    "^😐😐😐",
+   "^😐😐😐😐",
+    "^>😐😐😐😐😐",
 },
 run = run
 }
